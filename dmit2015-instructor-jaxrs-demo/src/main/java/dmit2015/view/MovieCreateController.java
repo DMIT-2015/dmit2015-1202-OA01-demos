445efba85185349d5faa7f9a4e472f4fa144c524
Sam Wu <swu@nait.ca>
2021-03-04 09:40:05 -0700

    add JSF create movie page

---
+++ dmit2015-instructor-jaxrs-demo/src/main/java/dmit2015/view/MovieCreateController.java
@@ -0,0 +1,35 @@
+package dmit2015.view;
+
+import dmit2015.entity.Movie;
+import dmit2015.repository.MovieRepository;
+import lombok.Getter;
+import org.omnifaces.util.Messages;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("currentMovieCreateController")
+@RequestScoped
+public class MovieCreateController {
+
+    @Inject
+    private MovieRepository _movieRepository;
+
+    @Getter
+    private Movie newMovie = new Movie();
+
+    public String onCreateNew() {
+        String nextPage = "";
+        try {
+            _movieRepository.add(newMovie);
+            Messages.addFlashGlobalInfo("Create was successful.");
+            nextPage = "index?faces-redirect=true";
+        } catch (Exception e) {
+            e.printStackTrace();
+            Messages.addGlobalError("Create was not successful.");
+        }
+        return nextPage;
+    }
+
+}
