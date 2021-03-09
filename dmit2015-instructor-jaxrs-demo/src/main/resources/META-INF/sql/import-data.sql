129f2e47cf8fa361a2876197df89569440f5f4be
Sam Wu <swu@nait.ca>
2021-03-09 10:17:32 -0700

    added CRUD pages for Movie

--- dmit2015-instructor-jaxrs-demo/src/main/resources/META-INF/sql/import-data.sql
+++ dmit2015-instructor-jaxrs-demo/src/main/resources/META-INF/sql/import-data.sql
@@ -1,8 +1,8 @@
-INSERT INTO TodoItem(name, complete) VALUES ('Todo 1', false);
-INSERT INTO TodoItem(name, complete) VALUES ('Todo 2', false);
-INSERT INTO TodoItem(name, complete) VALUES ('Todo 3', false);
+INSERT INTO TodoItem(name, complete,version) VALUES ('Todo 1', false,0);
+INSERT INTO TodoItem(name, complete,version) VALUES ('Todo 2', false,0);
+INSERT INTO TodoItem(name, complete,version) VALUES ('Todo 3', false,0);
 
-INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime) VALUES('When Harry Met Sally','1989-2-12','Romantic Comedy',7.99,'G','2021-02-25T08:00','2021-02-25T08:15');
-INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime) VALUES('Ghostbusters','1984-3-13','Comedy',8.99,'PG','2021-02-25T08:00','2021-02-25T08:15');
-INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime) VALUES('Ghostbusters 2','1986-2-23','Comedy',9.99,'PG','2021-02-25T08:00','2021-02-25T08:15');
-INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime) VALUES('Rio Bravo','1959-4-15','Western',7.99,'PG-13','2021-02-25T08:00','2021-02-25T08:15');
+INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime,version) VALUES('When Harry Met Sally','1989-2-12','Romantic Comedy',7.99,'G','2021-02-25T08:00','2021-02-25T08:15',0);
+INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime,version) VALUES('Ghostbusters','1984-3-13','Comedy',8.99,'PG','2021-02-25T08:00','2021-02-25T08:15',0);
+INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime,version) VALUES('Ghostbusters 2','1986-2-23','Comedy',9.99,'PG','2021-02-25T08:00','2021-02-25T08:15',0);
+INSERT INTO movies(title,releaseDate,genre,price,rating,createdDateTime,lastModifiedDateTime,version) VALUES('Rio Bravo','1959-4-15','Western',7.99,'PG-13','2021-02-25T08:00','2021-02-25T08:15',0);
